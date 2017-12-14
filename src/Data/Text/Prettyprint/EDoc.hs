5dc445a8d478d0efbed7369955d57ecc8265f333
dcastrop <d.castro-perez@imperial.ac.uk>
2017-12-14 17:27:02 +0000

    Fixes:
      * It makes no sense to reexport Data.Text.Prettyprint.Doc!

--- src/Data/Text/Prettyprint/EDoc.hs
+++ src/Data/Text/Prettyprint/EDoc.hs
@@ -1,9 +1,7 @@
 module Data.Text.Prettyprint.EDoc
 ( EDoc
 , ppr
-, module X
 ) where
 
 import Data.Text.Prettyprint.EDoc.Internal ( EDoc )
 import Data.Text.Prettyprint.EDoc.Q ( ppr )
-import Data.Text.Prettyprint.Doc as X
